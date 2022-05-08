e506c9d3811272199bf270366dac81c3c942d23e
Oleksii Kutuzov <oleksii.kutuzov@icloud.com>
2022-05-08 03:13:16 +0200

    Implement solid rainbow in HSV with variable V

--- led_strip/led_strip.ino
+++ led_strip/led_strip.ino
@@ -234,31 +234,9 @@ struct Pixel_Strand : Service::LightBulb { // Addressable RGBW Pixel Strand of n
 		}
 
 		uint32_t update() override {
+			float value = px->V.getNewVal<float>();
 			for (int i = 0; i < px->nPixels; i++) {
-				int red, green, blue;
-
-				if (angle < 60) {
-					red	  = 255;
-					green = round(angle * 4.25 - 0.01);
-					blue  = 0;
-				} else if (angle < 120) {
-					red	  = round((120 - angle) * 4.25 - 0.01);
-					green = 255;
-					blue  = 0;
-				} else if (angle < 180) {
-					red = 0, green = 255;
-					blue = round((angle - 120) * 4.25 - 0.01);
-				} else if (angle < 240) {
-					red = 0, green = round((240 - angle) * 4.25 - 0.01);
-					blue = 255;
-				} else if (angle < 300) {
-					red = round((angle - 240) * 4.25 - 0.01), green = 0;
-					blue = 255;
-				} else {
-					red = 255, green = 0;
-					blue = round((360 - angle) * 4.25 - 0.01);
-				}
-				px->colors[i] = Pixel::Color().RGB(red, green, blue, 0);
+				px->colors[i] = Pixel::Color().HSV(angle, 100, value);
 			}
 			px->pixel->set(px->colors, px->nPixels);
 			angle++;
